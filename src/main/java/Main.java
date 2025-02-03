91b4b5b3060d34097fbb1ce1819941b3a2321821
Olga <olakostina31@gmail.com>
2025-02-03 16:24:09 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    Server server = new Server(9999, validPaths, 64);
+    server.start();
+  }
+}
+
+
